4e2275e02da742678e5064a91cb35924f095479f
Prabhu-1409 <msprabhuraj15@gmail.com>
2024-09-28 19:09:35 +0530

    about page

--- src/About.js
+++ src/About.js
@@ -6,6 +6,25 @@ function About(){
     return(
         <div className="about">
             <div className="circle1"></div>
+            <div className='about-inner'>
+                <div className="about-letter">
+                    <h4 className="about-spell">.../About me ...</h4>
+                </div>
+                <div className="info">
+                    <p className="info-para">I figured it out in the end but will leave this here, as it might be useful for someone else!
+                        The error was associated with the R code that was in the command, so if this occurs to you as well, focus in on the R-part of your code rather than on the workflow code</p>
+                </div>
+            </div>
+            <div className="skills">
+                <div>
+                <div className="programming_languages">
+
+                </div>
+                </div>
+                <div className="image">
+
+                </div>
+            </div>
         </div>
     )
 }
